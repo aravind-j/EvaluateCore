e49557727bc4a45f82a5e853e9686c5607761b99
J. Aravind <aravindjayaramanagri@gmail.com>
2018-11-26 01:15:25 +0530

    Added examples

--- R/shannon.evaluate.core.R
+++ R/shannon.evaluate.core.R
@@ -24,6 +24,36 @@
 #'
 #' @examples
 #'
+#' ####################################
+#' # Use data from R package ccChooser
+#' ####################################
+#'
+#' library(ccChooser)
+#' data("dactylis_CC")
+#' data("dactylis_EC")
+#'
+#' ec <- cbind(genotypes = rownames(dactylis_EC), dactylis_EC[, -1])
+#' ec$genotypes <- as.character(ec$genotypes)
+#' rownames(ec) <- NULL
+#' ec[, c("X1", "X6", "X7")] <- lapply(ec[, c("X1", "X6", "X7")],
+#'                                     function(x) cut(x, breaks = 4))
+#' ec[, c("X1", "X6", "X7")] <- lapply(ec[, c("X1", "X6", "X7")],
+#'                                     function(x) factor(as.numeric(x)))
+#' head(ec)
+#'
+#' core <- rownames(dactylis_CC)
+#'
+#' quant <- c("X2", "X3", "X4", "X5", "X8")
+#' qual <- c("X1", "X6", "X7")
+#'
+#' ####################################
+#' # EvaluateCore
+#' ####################################
+#'
+#' shannon.evaluate.core(data = ec, names = "genotypes",
+#'                       qualitative = qual, selected = core)
+#'
+#'
 shannon.evaluate.core <- function(data, names, qualitative, selected){
   # Checks
   checks.evaluate.core(data = data, names = names,
@@ -49,19 +79,19 @@ shannon.evaluate.core <- function(data, names, qualitative, selected){
 
   # dataf[, qualitative] <- lapply(dataf[, qualitative], as.integer)
 
-  EC_H <- psych::shannon(dataf[dataf$`[Type]` == "EC", !colnames(dataf) %in% c("Accession", "`[Type]`")],
+  EC_H <- psych::shannon(dataf[dataf$`[Type]` == "EC", !colnames(dataf) %in% c(names, "[Type]")],
                          base =  exp(1), correct = FALSE)
-  CS_H <- psych::shannon(dataf[dataf$`[Type]` == "CS", !colnames(dataf) %in% c("Accession", "`[Type]`")],
+  CS_H <- psych::shannon(dataf[dataf$`[Type]` == "CS", !colnames(dataf) %in% c(names, "[Type]")],
                          base =  exp(1), correct = FALSE)
 
-  EC_EH <- psych::shannon(dataf[dataf$`[Type]` == "EC", !colnames(dataf) %in% c("Accession", "`[Type]`")],
+  EC_EH <- psych::shannon(dataf[dataf$`[Type]` == "EC", !colnames(dataf) %in% c(names, "[Type]")],
                          base =  exp(1), correct = TRUE)
-  CS_EH <- psych::shannon(dataf[dataf$`[Type]` == "CS", !colnames(dataf) %in% c("Accession", "`[Type]`")],
+  CS_EH <- psych::shannon(dataf[dataf$`[Type]` == "CS", !colnames(dataf) %in% c(names, "[Type]")],
                           base =  exp(1), correct = TRUE)
 
-  EC_No.Classes <- unlist(lapply(dataf[dataf$`[Type]` == "EC", !colnames(dataf) %in% c("Accession", "`[Type]`")],
+  EC_No.Classes <- unlist(lapply(dataf[dataf$`[Type]` == "EC", !colnames(dataf) %in% c(names, "[Type]")],
                           function(x) length(levels(droplevels(x)))))
-  CS_No.Classes <- unlist(lapply(dataf[dataf$`[Type]` == "CS", !colnames(dataf) %in% c("Accession", "`[Type]`")],
+  CS_No.Classes <- unlist(lapply(dataf[dataf$`[Type]` == "CS", !colnames(dataf) %in% c(names, "[Type]")],
                           function(x) length(levels(droplevels(x)))))
 
   outdf <- cbind(EC_H = EC_H[qualitative],
