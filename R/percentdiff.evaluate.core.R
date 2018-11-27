69ce21753ae22263bd46696bf05da91ee5ea7523
J. Aravind <aravindjayaramanagri@gmail.com>
2018-11-27 16:00:50 +0530

    Added method of Kim

--- R/percentdiff.evaluate.core.R
+++ R/percentdiff.evaluate.core.R
@@ -1,9 +1,72 @@
 #' Percentage Difference of Means and Variances
 #'
-#' Compute the percentage of significant differences between the entire
-#' collection (EC) and core set (CS) as Mean difference percentage (MD) and
-#' Variance difference percentage (VD)
-#' \insertCite{hu_methods_2000}{EvaluateCore}.
+#' Compute the following differences between the entire collection (EC) and core
+#' set (CS). \itemize{ \item Percentage of significant differences of mean
+#' (\ifelse{html}{\out{<em>MD\%<sub>Hu</sub></em>}}{\eqn{MD\%_{Hu}}})
+#' \insertCite{hu_methods_2000}{EvaluateCore} \item Percentage of significant
+#' differences of variance
+#' (\ifelse{html}{\out{<em>VD\%<sub>Hu</sub></em>}}{\eqn{VD\%_{Hu}}})
+#' \insertCite{hu_methods_2000}{EvaluateCore} \item Average of absolute
+#' differences between means
+#' (\ifelse{html}{\out{<em>MD\%<sub>Kim</sub></em>}}{\eqn{MD\%_{Kim}}})
+#' \insertCite{kim_powercore:_2007}{EvaluateCore} \item Average of absolute
+#' differences between variances
+#' (\ifelse{html}{\out{<em>VD\%<sub>Kim</sub></em>}}{\eqn{VD\%_{Kim}}})
+#' \insertCite{kim_powercore:_2007}{EvaluateCore} }
+#'
+#' The differences are computed as follows.
+#'
+#' \ifelse{html}{\out{<p style="text-align: center;"><em>MD\%<sub>Hu</sub></em>
+#' = <big>(</big><sup>S<sub>t</sub></sup> &frasl; <sub>n</sub><big>)</big>
+#' &times; 100</p>}}{\deqn{MD\%_{Hu} = \left ( \frac{S_{t}}{n} \right ) \times
+#' 100}}
+#'
+#' Where \ifelse{html}{\out{<em>S<sub>t</sub></em>}}{\eqn{S_{t}}} is the number
+#' of traits with a significant difference between the means of the EC and the
+#' CS and \ifelse{html}{\out{<em>n</em>}}{\eqn{n}} is the total number of
+#' traits.
+#'
+#' \ifelse{html}{\out{<p style="text-align: center;"><em>VD\%<sub>Hu</sub></em>
+#' = <big>(</big><sup>S<sub>t</sub></sup> &frasl; <sub>n</sub><big>)</big>
+#' &times; 100</p>}}{\deqn{VD\%_{Hu} = \left ( \frac{S_{t}}{n} \right ) \times
+#' 100}}
+#'
+#' Where \ifelse{html}{\out{<em>S<sub>t</sub></em>}}{\eqn{S_{t}}} is the number
+#' of traits with a significant difference between the variances of the EC and
+#' the CS and \ifelse{html}{\out{<em>n</em>}}{\eqn{n}} is the total number of
+#' traits.
+#'
+#' \ifelse{html}{\out{<p style="text-align: center;"><em>MD\%<sub>Kim</sub> =
+#' <sup>1</sup>&frasl;<sub>n</sub> &sum;<sup>n</sup><sub style="line-height:
+#' 1.8; margin-left: -1ex;">i=1</sub> <big>[</big>
+#' <sup>|M<sub>EC<sub>i</sub></sub> &minus; M<sub>CS<sub>i</sub></sub>|</sup>
+#' &frasl; <sub>M<sub>CS<sub>i</sub></sub></sub> <big>]</big>  &times;
+#' 100</em></p>}}{\deqn{MD\%_{Kim} = \frac{1}{n}\sum_{i=1}^{n} \frac{\left |
+#' M_{EC_{i}}-M_{CS_{i}} \right |}{M_{CS_{i}}}}}
+#'
+#' Where
+#' \ifelse{html}{\out{<em>M<sub>EC<sub>i</sub></sub></em>}}{\eqn{M_{EC_{i}}}} is
+#' the mean of the EC for the \ifelse{html}{\out{<em>i</em>}}{\eqn{i}}th trait,
+#' \ifelse{html}{\out{<em>M<sub>CS<sub>i</sub></sub></em>}}{\eqn{M_{CS_{i}}}} is
+#' the mean of the CS for the \ifelse{html}{\out{<em>i</em>}}{\eqn{i}}th trait
+#' and \ifelse{html}{\out{<em>n</em>}}{\eqn{n}} is the total number of traits.
+#'
+#' \ifelse{html}{\out{<p style="text-align: center;"><em>VD\%<sub>Kim</sub> =
+#' <sup>1</sup>&frasl;<sub>n</sub> &sum;<sup>n</sup><sub style="line-height:
+#' 1.8; margin-left: -1ex;">i=1</sub> <big>[</big>
+#' <sup>|V<sub>EC<sub>i</sub></sub> &minus; V<sub>CS<sub>i</sub></sub>|</sup>
+#' &frasl; <sub>V<sub>CS<sub>i</sub></sub></sub> <big>]</big>  &times;
+#' 100</em></p>}}{\deqn{VD\%_{Kim} = \frac{1}{n}\sum_{i=1}^{n} \frac{\left |
+#' V_{EC_{i}}-V_{CS_{i}} \right |}{V_{CS_{i}}}}}
+#'
+#' Where
+#' \ifelse{html}{\out{<em>V<sub>EC<sub>i</sub></sub></em>}}{\eqn{V_{EC_{i}}}} is
+#' the variance of the EC for the \ifelse{html}{\out{<em>i</em>}}{\eqn{i}}th
+#' trait,
+#' \ifelse{html}{\out{<em>V<sub>CS<sub>i</sub></sub></em>}}{\eqn{V_{CS_{i}}}} is
+#' the variance of the CS for the \ifelse{html}{\out{<em>i</em>}}{\eqn{i}}th
+#' trait and \ifelse{html}{\out{<em>n</em>}}{\eqn{n}} is the total number of
+#' traits.
 #'
 #' @inheritParams snk.evaluate.core
 #' @param alpha Type I error probability (Significance level) of difference.
@@ -73,8 +136,10 @@ percentdiff.evaluate.core <- function(data, names, quantitative,
   vdiff <- levene.evaluate.core(data, names, quantitative,
                                 selected)
 
-  outdf <- data.frame(MDPercent = (sum(mdiff$SNK_pvalue <= alpha)/ length(quantitative) * 100),
-                      VDPercent = (sum(vdiff$Levene_pvalue <= alpha)/ length(quantitative) * 100))
+  outdf <- data.frame(MDPercent_Hu = (sum(mdiff$SNK_pvalue <= alpha)/ length(quantitative)) * 100,
+                      VDPercent_Hu = (sum(vdiff$Levene_pvalue <= alpha)/ length(quantitative)) * 100,
+                      MDPercent_Kim = (sum(abs(mdiff$EC_Mean - mdiff$CS_Mean)/mdiff$CS_Mean)/length(quantitative)) * 100,
+                      VDPercent_Kim = (sum(abs(vdiff$EC_V - vdiff$CS_V)/vdiff$CS_V)/length(quantitative)) * 100)
 
   return(outdf)
 
