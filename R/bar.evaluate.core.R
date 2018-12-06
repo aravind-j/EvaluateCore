649c5a4c9a89422171d5ddb78333d74970980ea0
J. Aravind <aravindjayaramanagri@gmail.com>
2018-12-06 16:54:11 +0530

    x axis angle change

--- R/bar.evaluate.core.R
+++ R/bar.evaluate.core.R
@@ -1,3 +1,21 @@
+### This file is part of 'EvaluateCore' package for R.
+
+### Copyright (C) 2018, ICAR-NBPGR.
+#
+# EvaluateCore is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# EvaluateCore is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  https://www.r-project.org/Licenses/
+
+
 #' Bar Plots
 #'
 #' Plot Bar plots to graphically compare the frequency distributions of
@@ -59,7 +77,7 @@ bar.evaluate.core <- function(data, names, qualitative, selected){
 
   dataf <- data[, c(names, qualitative)]
 
-  datafcore <- dataf[dataf[,names] %in% selected,]
+  datafcore <- dataf[dataf[, names] %in% selected, ]
 
   dataf$`[Type]` <- "EC"
   datafcore$`[Type]` <- "CS"
@@ -82,7 +100,8 @@ bar.evaluate.core <- function(data, names, qualitative, selected){
       xlab(qualitative[i]) +
       facet_grid(~`[Type]`) +
       theme_bw() +
-      theme(axis.text = element_text(colour = "black"))
+      theme(axis.text = element_text(colour = "black"),
+            axis.text.x = element_text(angle = 45, hjust = 1))
 
   }
 
