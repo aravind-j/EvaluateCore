3116894fbeac9d1e020604d1f7e8931924ee58e8
J. Aravind <aravindjayaramanagri@gmail.com>
2022-06-11 22:38:15 +0200

    Update copyright year

--- R/corr.evaluate.core.R
+++ R/corr.evaluate.core.R
@@ -1,6 +1,6 @@
 ### This file is part of 'EvaluateCore' package for R.
 
-### Copyright (C) 2018-2021, ICAR-NBPGR.
+### Copyright (C) 2018-2022, ICAR-NBPGR.
 #
 # EvaluateCore is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
